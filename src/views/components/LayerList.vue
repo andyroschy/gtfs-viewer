5843e5580621e8d927ff38ccd981b53b45961082
Andres <andyroschy@hotmail.com>
2018-09-28 04:55:41 -0300

    add checkbox to toggle layer visibility

--- src/views/components/LayerList.vue
+++ src/views/components/LayerList.vue
@@ -3,13 +3,17 @@
     <ul >
       <!-- list of agencies -->
       <li v-for="agency of agencies" :key="agency.id" >
-          <span @click="toggleLayer(agency)"  class="layer-name" >{{agency.name}} - Routes:</span>     
+          <input type="checkbox" v-model="agency.visible">
+          <span @click="toggleLayer(agency)"  class="layer-name" >{{agency.name}} </span>     
           <!-- routes of the agency -->
-          <ul class="inner" :class="{'open': agency.isOpen}" >
+          <ul class="inner" :class="{'open': agency.isOpen}" >            
+            <li class="sub-menu-header">Routes:</li>
             <li   v-for="route of agency.routes" :key="route.id">
-              <span @click="toggleLayer(route)" class="layer-name" >{{route.name}} - Stops:</span>
+              <input type="checkbox" v-model="route.visible">
+              <span @click="toggleLayer(route)" class="layer-name" >{{route.name}}</span>
               <!-- stops on the route -->
               <ul class="inner" :class="{'open': route.isOpen}"> 
+                <li class="sub-menu-header">Stops:</li>
                 <li v-for="stop of route.stops" :key="stop.id">{{stop.name}}</li>
               </ul>
             </li>
@@ -30,6 +34,9 @@
       list-style: none;
       span.layer-name {
         cursor: pointer;
+      }
+      li.sub-menu-header {
+        margin-left: -10px;
       }
       ul.inner {
         //transform: scaleY(0);

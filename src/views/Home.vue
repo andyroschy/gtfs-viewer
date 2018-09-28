30c494094e9d6939b9890dc9a2b96d29e7af41c1
Andres <andyroschy@hotmail.com>
2018-09-28 03:06:31 -0300

    move map to own component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,7 @@
 <template>
   <div class="main">
     <layer-list :agencies="agencies" ></layer-list>
-    <l-map :zoom="zoom" :center="center" class="main-map">
-        <l-tile-layer :url="baseLayer" :attribution="attribution"></l-tile-layer>
-        <!-- Agency layers -->
-        <l-layer-group
-            v-for="agency in agencies"
-            :key="agency.id"
-            :visible="agency.visible"
-            layerType="overlay"
-            :name="agency.name">
-            <!-- Route layers -->
-          <l-layer-group 
-              v-for="route in agency.routes"
-              :key="route.id"
-              :visible="route.visible"
-              layerType="overlay"
-              :name="route.name" >
-              <!-- route line -->
-              <l-polyline
-                :key="route.id"
-                :lat-lngs="route.geometry"
-                :visible="route.visible"/>
-                <!-- route stops -->
-              <l-marker
-                v-for="stop in route.stops"
-                :key="stop.id"
-                :visible="stop.visible"
-                :lat-lng="stop.latlng"
-                :icon="markerIcon"
-                @click="alert(stop.name)" />
-          </l-layer-group>
-        </l-layer-group>
-       
-        <!-- <l-marker :lat-lng="marker" :icon="leafIcon"></l-marker>
-        <l-marker v-for="stop in stops" :key="stop.id" :lat-lng="stop.coords" :icon="leafIcon"></l-marker> -->
-    </l-map>
+    <main-map :agencies="agencies"></main-map>
     <input type="file" @change="fileSelected" multiple/>
     <span>{{loadStatus}}</span>
     <br/>
@@ -46,16 +12,12 @@
 
 <style scoped>
 
-  .main-map {
-    height: 500px;
-    width: 100%;
-  }
-
 </style>
 
 
 <script lang="ts">
 import LayerList  from '@/views/components/LayerList.vue';
+import MainMap  from '@/views/components/MainMap.vue';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import {LMap, LMarker, LTileLayer, LPolyline, LLayerGroup} from 'vue2-leaflet';
 import L, { LatLng, imageOverlay } from 'leaflet';
@@ -69,22 +31,12 @@ import jszip from 'jszip'
 
 @Component({
   components: {
-    LMap, LMarker, LTileLayer, LPolyline, LLayerGroup, LayerList
+     LayerList, MainMap
   },
 })
-export default class Home extends Vue {
-  public markerIcon = marker;
-  public zoom: number = 10;
-  public center: LatLng = L.latLng(36.70365959719456, -116.70364379882814);
-  //public marker: LatLng =  L.latLng(47.413220, -1.219482);
-  public baseLayer: string = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-  public attribution: string = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-  public agencies: AgencyLayer[] = [];
+export default class Home extends Vue {  
+  public agencies: AgencyLayer[] = [];  
   public loadStatus: string = '';
-
-  public alert(value: string) {
-    alert(value);
-  }
 
   public created() {
     const self = this;
---
+++ src/views/components/MainMap.vue
@@ -0,0 +1,80 @@
+<template>
+  <l-map :zoom="zoom" :center="center" class="main-map">
+        <l-tile-layer :url="baseLayer" :attribution="attribution"></l-tile-layer>
+        <!-- Agency layers -->
+        <l-layer-group
+            v-for="agency in agencies"
+            :key="agency.id"
+            :visible="agency.visible"
+            layerType="overlay"
+            :name="agency.name">
+            <!-- Route layers -->
+          <l-layer-group 
+              v-for="route in agency.routes"
+              :key="route.id"
+              :visible="route.visible"
+              layerType="overlay"
+              :name="route.name" >
+              <!-- route line -->
+              <l-polyline
+                :key="route.id"
+                :lat-lngs="route.geometry"
+                :visible="route.visible"/>
+                <!-- route stops -->
+              <l-marker
+                v-for="stop in route.stops"
+                :key="stop.id"
+                :visible="stop.visible"
+                :lat-lng="stop.latlng"
+                :icon="markerIcon"
+                @click="stopClicked(stop.name)" />
+          </l-layer-group>
+        </l-layer-group>
+    </l-map>
+</template>
+
+<style scoped>
+
+  .main-map {
+    height: 500px;
+    width: 100%;
+  }
+
+</style>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import {LMap, LMarker, LTileLayer, LPolyline, LLayerGroup} from 'vue2-leaflet';
+import L, { LatLng, imageOverlay } from 'leaflet';
+import { marker } from '@/leflet-icons';
+import { getTestFeed, parseFeed, getFeedFromFile } from '@/services/feed-service';
+import { getAgencies } from '@/services/layer-service';
+import { normalize } from 'path';
+import AgencyLayer from '@/types/agency-layer';
+
+
+@Component({
+  components: {
+    LMap, LMarker, LTileLayer, LPolyline, LLayerGroup
+  },
+})
+export default class MainMap extends Vue {
+  public markerIcon = marker;
+  public zoom: number = 10;
+  public center: LatLng = L.latLng(36.70365959719456, -116.70364379882814);
+  //public marker: LatLng =  L.latLng(47.413220, -1.219482);
+  public baseLayer: string = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+  public attribution: string = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+  @Prop()
+  public agencies!: AgencyLayer[];
+
+  public stopClicked(value: string) {
+    alert(value);
+  }
+
+  public created() {
+  }
+
+}
+</script>
